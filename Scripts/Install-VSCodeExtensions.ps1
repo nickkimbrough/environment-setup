947e4e1441a2fc0d194a96511d292ce2b8e880bb
Nick Kimbrough <nickkimbrough@gmail.com>
2021-01-27 21:10:49 -0700

    Add script to install vscode extensions

---
+++ Scripts/Install-VSCodeExtensions.ps1
@@ -0,0 +1,7 @@
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension ms-vscode.powershell
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension redhat.vscode-yaml
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension oderwat.indent-rainbow
